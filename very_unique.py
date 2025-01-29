e7f09fe5bd09fbab3537ef1268dc93b048a2249f
praerie <elisabethadelie@gmail.com>
2025-01-28 20:11:51 -0500

    create remove_duplicates function

--- very_unique.py
+++ very_unique.py
@@ -1,3 +1,43 @@
-# Write and test a function removeDuplicates(somelist) 
+# Write and test a function removeDuplicates(unique_list)
 # that removes duplicate values from a list.
+
+def remove_duplicates(unique_list):
+    """
+    Removes duplicate items from the given list.
+
+    Args:
+        unique_list (list): Input list that may contain duplicate values.
+
+    Returns:
+        result (list): A new list with duplicates removed.
+    """
+    # initialize an empty set to keep track of seen items
+    seen = set()
+    
+    # initialize an empty list to store unique items in the order they appear
+    result = []
+    
+    for item in unique_list:
+        if item not in seen:
+            result.append(item)
+            seen.add(item)
+    return result
+
+def main():
+    herbs = ["thyme", "sumac", "tarragon", "dill", "dill", "black pepper", "sumac", "marjoram"]
+    studios = ["Dreamworks", "Nickelodeon", "Disney", "Sony", "Sony", "ILM", "Animal Logic", "ILM", "ILM"]
+    hobbies = ["archery", "archery", "archery", "knitting", "language learning", "mountain biking"]
+
+    print("original list of herbs:", herbs)
+    print("post duplicate removal:", removeDuplicates(herbs), "\n")
+
+    print("original list of studios", studios)
+    print("post duplicate removal:", removeDuplicates(studios), "\n")
+
+    print("original list of hobbies:", hobbies)
+    print("post duplicate removal:", removeDuplicates(hobbies))
+
+if __name__ == "__main__":
+    main()
+
 
