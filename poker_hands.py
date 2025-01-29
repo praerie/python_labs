5b0571d475a834cc826123ac7684b768b936abc6
praerie <elisabethadelie@gmail.com>
2025-01-28 22:24:22 -0500

    add straight_flush check

--- poker_hands.py
+++ poker_hands.py
@@ -11,3 +11,78 @@ def main():
           
 if __name__ == "__main__":
     main()
+def make_deck():
+    """Creates a standard deck of 52 cards as tuples (rank, suit)."""
+    suits = ["c", "d", "h", "s"] # clubs, diamonds, hearts, spades
+    ranks = list(range(2, 15)) # 2-10, jack (11), queen (12), king (13), ace (14)
+    
+    return [(rank, suit) for suit in suits for rank in ranks]
+
+def straight_flush(cards):
+    """Returns true if hand has five ranks in a row of all the same suit."""
+    if len(cards) != 5:
+        return False # must be 5 cards
+    
+    # create a set of unique suits from list of card tuples
+    suits = {suit for _, suit in cards}
+    
+    if len(suits) > 1:
+        return False # must be same suit
+    
+    # unpack and sort ranks
+    ranks = [rank for rank, _ in cards]
+    ranks.sort()
+    
+    # check if ranks form consecutive sequence
+    if all(ranks[i] + 1 == ranks[i + 1] for i in range(len(ranks) - 1)):
+        return True
+
+def four_of_a_kind(cards):
+    """Returns true if hand has four of the same rank."""
+    pass
+
+def full_house(cards):
+    """Returns true if hand has three of one rank and two of another."""
+    pass
+
+def flush(cards):
+    """Returns true if hand has five cards of the same suit."""
+    pass
+
+def straight(cards):
+    """Returns true if hand has five ranks in a row."""
+    pass
+
+def three_of_a_kind(cards):
+    """Returns true if hand has three of one rank,
+    but not a full house nor four of a kind."""
+    pass
+
+def two_pair(cards):
+    """Returns true if hand has two each of two different ranks."""
+    pass
+
+def pair(cards):
+    """Returns true if hand has two of the same rank,
+    but not two pair, three, or four of a kind."""
+    pass
+          
+def main():
+    deck = make_deck()
+    print(f"standard 52-card deck: {deck}")
+    
+    hand_a = [(2, "s"), (4, "s"), (6, "s"), (5, "s"), (3, "s")]
+    hand_b = [(7, "c"), (7, "c"), (7, "c"), (7, "c"), (3, "c")]
+    hand_c = [(12, "d"), (12, "h"), (12, "h"), (9, "h"), (9, "d")]
+    
+    print(f"straight flush: {hand_a}, {straight_flush(hand_a)}")
+    print(f"four of a kind: {hand_b}")
+    print(f"full house: {hand_c}")
+    print(f"flush: ")
+    print(f"straight: ")
+    print(f"three of a kind: ")
+    print(f"two pair: ")
+    print(f"pair: ")
+          
+if __name__ == "__main__":
+    main()
