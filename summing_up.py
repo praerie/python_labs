31dfcb293e4a77feac6d8707db9324d6b2076f66
praerie <elisabethadelie@gmail.com>
2025-02-02 20:03:50 -0500

    set up summing_up exercise

---
+++ summing_up.py
@@ -0,0 +1,9 @@
+# Write definitions for the following two functions: 
+
+# sum_n(n) returns the sum of the first n natural numbers. 
+# sum_n_cubes(n) returns the sum of the cubes of the first n natural numbers.
+
+# Then use these funcftions in a program that prompts a user for an n 
+# and prints out the sum of the first n natural numbers 
+# and the sum of the cubes of the first n natural numbers. 
+
